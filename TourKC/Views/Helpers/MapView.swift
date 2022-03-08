1003b1bfa81b92af33d9aa52130065d690ed55ac
jhawgs <70661513+jhawgs@users.noreply.github.com>
2022-03-07 20:01:47 -0600

    made coordinates more specific

--- TourKC/Views/Helpers/MapView.swift
+++ TourKC/Views/Helpers/MapView.swift
@@ -22,7 +22,7 @@ struct MapView: View {
     private func setRegion(_ coordinate: CLLocationCoordinate2D) {
         region = MKCoordinateRegion(
             center: coordinate,
-            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+            span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
         )
     }
 }
